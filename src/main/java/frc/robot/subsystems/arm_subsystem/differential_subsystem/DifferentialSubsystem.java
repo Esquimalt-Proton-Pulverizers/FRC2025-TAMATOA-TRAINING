5125877354b6ff9c5e076bf99dd626b845223e71
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2025-08-02 15:15:36 -0700

    Buildable base code
    Refactored ManipulatorSubsystem to DifferentialSubsystem.

--- src/main/java/frc/robot/subsystems/arm_subsystem/differential_subsystem/DifferentialSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm_subsystem/differential_subsystem/DifferentialSubsystem.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.arm_subsystem.manipulator_subsystem.intake_subsystem;
+package frc.robot.subsystems.arm_subsystem.differential_subsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase {
+public class DifferentialSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public IntakeSubsystem() {}
+  public DifferentialSubsystem() {}
 
   /**
    * Example command factory method.
