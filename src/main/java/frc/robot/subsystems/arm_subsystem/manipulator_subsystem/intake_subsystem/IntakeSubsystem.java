ef179a3779f5f2bc1a9c2766c02ad2ae0de6387b
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2025-08-02 14:27:39 -0700

    Edited files to fix package errors

--- src/main/java/frc/robot/subsystems/arm_subsystem/manipulator_subsystem/intake_subsystem/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm_subsystem/manipulator_subsystem/intake_subsystem/IntakeSubsystem.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.arm_subsystem.manipulator_subsystem.intake_subsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public IntakeSubsystem() {}
 
   /**
    * Example command factory method.
